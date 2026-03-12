064ac01ae0dc21d1e35060d6de41e1619755443d
Marco Volkert <marco.volkert@astrum-it.de>
2026-03-12 11:37:56 +0100

    Initial commit: Add Maven project structure for note2mermaid

---
+++ src/main/java/com/mvolkert/note2mermaid/Note2mermaidApplication.java
@@ -0,0 +1,13 @@
+package com.mvolkert.note2mermaid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Note2mermaidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Note2mermaidApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mvolkert/note2mermaid/Note2mermaidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mvolkert.note2mermaid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Note2mermaidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
